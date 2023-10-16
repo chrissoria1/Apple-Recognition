7d8fd9fbeb6352732ad63021bfd85bf16b2c83b5
Chris A Soria <soriac1@southernct.edu>
2023-10-16 17:32:06 -0400

    Initial Commit

---
+++ finaltest/finaltestApp.swift
@@ -0,0 +1,17 @@
+//
+//  finaltestApp.swift
+//  finaltest
+//
+//  Created by Chris Soria on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct finaltestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
